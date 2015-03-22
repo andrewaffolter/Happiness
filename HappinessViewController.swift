41ae88ef72d50716ba2dcf0a3df69d7316c1c048
Andrew Affolter <andrewaffolter@gmail.com>
2015-03-21 20:08:32 -0700

    Inital commit of this project

---
+++ HappinessViewController.swift
@@ -0,0 +1,14 @@
+//
+//  HappinessViewController.swift
+//  Happiness
+//
+//  Created by Andrew Affolter on 3/21/15.
+//  Copyright (c) 2015 Andrew Affolter. All rights reserved.
+//
+
+import UIKit
+
+class HappinessViewController: UIViewController {
+
+
+}
